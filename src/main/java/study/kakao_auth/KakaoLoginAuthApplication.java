89cd7bb8315f038ae1a951efd2b6983a09eb9cc7
hskhsmm <hskhsmm@naver.com>
2025-07-10 22:31:25 +0900

    first commit

---
+++ src/main/java/study/kakao_auth/KakaoLoginAuthApplication.java
@@ -0,0 +1,13 @@
+package study.kakao_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoLoginAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoLoginAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/kakao_auth/KakaoLoginAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kakao_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoLoginAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
