695db2c7122a95ab265007cf3dd8a0da7234fc9f
hskhsmm <hskhsmm@naver.com>
2025-07-12 01:08:29 +0900

    feat: 카카오 사용자 정보 응답 DTO 클래스 추가

--- src/main/java/study/kakao_auth/dto/KakaoTokenResponseDto.java
+++ src/main/java/study/kakao_auth/dto/KakaoTokenResponseDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true) // JSON에 정의되지 않은 속성이 있어도 무시하고 정상적으로 매핑
 public class KakaoTokenResponseDto {
 
-    // 역직렬화 목적: 카카오 서버에서 전달받은 JSON 응답을 자바 객체로 변환하기 위해 사용
+    // 역직렬화 목적: 카카오 서버에서 전달받은 JSON 응답을 자바 객체로 변환
     // 해당 필드들은 JSON 키와 일치하는 속성명을 @JsonProperty로 지정하여 자동 매핑
     @JsonProperty("token_type")
     public String tokenType;
